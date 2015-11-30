87e1cef798796283a075227886b58a71e8fcfdc4
thomasvier <thomasvier@gmail.com>
2015-11-30 14:31:46 -0200

    as

---
+++ PedidosWeb/Admin/Pedidos.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PedidosWeb.Admin
+{
+    public partial class Pedidos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSalvar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnPesquisar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvPedidos_Sorting(object sender, GridViewSortEventArgs e)
+        {
+
+        }
+
+        protected void gvPedidos_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+
+        }
+
+        protected void gvPedidos_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+        }
+    }
+}
